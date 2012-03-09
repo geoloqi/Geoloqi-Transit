c62ec80e59bce8fe0b881057bc9e8f6db1daba79
Kyle Drake <kyledrake@gmail.com>
2012-03-08 22:29:00 -0600

    Send message on trigger

--- controller.rb
+++ controller.rb
@@ -1,6 +1,23 @@
-
 require './environment.rb'
 
 post '/trigger' do
-  puts params.inspect
+  payload = Hashie::Mash.new JSON.parse(request.body.read)
+
+  stop_id = payload.place.extra
+
+  stop = Stop[agency: Agency[name: 'austin'], uid: stop_id] # TODO: Put agency in extra
+
+  session = Geoloqi::Session.new({
+    client_id: $config.geoloqi_client_id,
+    client_secret: $config.geoloqi_client_secret,
+    geoloqi_application_access_token: $config.geoloqi_application_access_token
+  })
+
+  session.post 'message/send', {
+    layer_id: payload.layer.layer_id,
+    user_id:  payload.user.user_id,
+    text:     stop.upcoming_times_message
+  }
+  # :url => "http://trimet.org/arrivals/small/tracker?locationID=#{body.place.extra.stopid}" IMPLEMENT THIS!!!
+  'ok'
 end
